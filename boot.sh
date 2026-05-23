e9ddbf44fe9f33093abb98e3c4e54787e4659268
Avik43218 <roychoudhuryavik005@gmail.com>
2026-05-23 22:12:45 +0530

    feat: Add a boot up script

---
+++ boot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CUBLAS_PATH=$(python3 -c "import nvidia.cublas.lib as lib; print(list(lib.__path__)[0])" 2>/dev/null)
+CUDNN_PATH=$(python3 -c "import nvidia.cudnn.lib as lib; print(list(lib.__path__)[0])" 2>/dev/null)
+
+export LD_LIBRARY_PATH="$CUBLAS_PATH:$CUDNN_PATH:$LD_LIBRARY_PATH"
+
+echo "[ LAUNCHING VICTUS MAINFRAME... ]"
+python3 jarvis.py
